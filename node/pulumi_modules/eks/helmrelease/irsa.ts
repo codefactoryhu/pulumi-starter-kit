5d288aa27ea84342724cb9adca8eb08895cce41c
vargha.laszlo <vargha.laszlo@codefactory.hu>
2023-12-12 11:34:33 +0100

    fix: add clusterrole for alb

--- node/pulumi_modules/eks/helmrelease/irsa.ts
+++ node/pulumi_modules/eks/helmrelease/irsa.ts
@@ -2,6 +2,8 @@ import * as pulumi  from "@pulumi/pulumi";
 import * as aws     from '@pulumi/aws';
 import * as k8s     from "@pulumi/kubernetes";
 
+import { createdCluster } from "../cluster/eks";
+
 export function iamRoleServiceAccount() {
     const assumeRole = aws.iam.getPolicyDocument({
         statements: [{
@@ -14,7 +16,10 @@ export function iamRoleServiceAccount() {
         }],
     });
 
-    const albIngressRole = new aws.iam.Role("pulumiAlbIngressRole", {assumeRolePolicy: assumeRole.then(assumeRole => assumeRole.json)});
+    const albIngressRole = new aws.iam.Role("aws-load-balancer-controller-role", {
+        name: "aws-load-balancer-controller-role",
+        assumeRolePolicy: assumeRole.then(assumeRole => assumeRole.json)
+    });
 
     const albInlinePolicy = aws.iam.getPolicyDocument({
             statements: [
@@ -270,7 +275,8 @@ export function iamRoleServiceAccount() {
                 }]
             });
 
-    const albPolicy = new aws.iam.Policy("pulumiAlbInlinePolicy", {
+    const albPolicy = new aws.iam.Policy("AWSLoadBalancerControllerIAMPolicy", {
+        name: "AWSLoadBalancerControllerIAMPolicy",
         description: "Policy for Pulumi EKS ALB Ingress Controller",
         policy: albInlinePolicy.then(albInlinePolicy => albInlinePolicy.json),
     });
@@ -279,4 +285,83 @@ export function iamRoleServiceAccount() {
         role: albIngressRole.name,
         policyArn: albPolicy.arn,
     });
+
+    createClusterRole(albIngressRole)
+}
+
+function createClusterRole(irsa:aws.iam.Role) {
+    const k8sProvider = new k8s.Provider("k8s-provider-irsa", {
+        kubeconfig: createdCluster.kubeconfig.apply(JSON.stringify),
+    });
+
+    const clusterRole = new k8s.rbac.v1.ClusterRole("dev-pulumi-clusterrole", {
+        metadata: {
+            name: "dev-pulumi-clusterrole",
+            namespace: "kube-system"
+        },
+        rules: [
+            {
+                apiGroups: ["elbv2.k8s.aws"],
+                resources: ["targetgroupbindings"],
+                verbs: ["create", "delete", "get", "list", "patch", "update", "watch"]
+            },
+            {
+                apiGroups: ["elbv2.k8s.aws"],
+                resources: ["ingressclassparams"],
+                verbs: ["get", "list", "watch"]
+            },
+            {
+                apiGroups: [""],
+                resources: ["events"],
+                verbs: ["create", "patch"]
+            },
+            {
+                apiGroups: [""],
+                resources: ["pods"],
+                verbs: ["get", "list", "watch"]
+            },
+            {
+                apiGroups: ["networking.k8s.io"],
+                resources: ["ingressclasses"],
+                verbs: ["get", "list", "watch"]
+            },
+            {
+                apiGroups: ["", "extensions", "networking.k8s.io"],
+                resources: ["services", "ingresses"],
+                verbs: ["get", "list", "patch", "update", "watch"]
+            },
+            {
+                apiGroups: [""],
+                resources: ["ndoes", "namespaces", "endpoints"],
+                verbs: ["get", "list", "watch"]
+            },
+            {
+                apiGroups: ["", "elbv2.k8s.aws", "extensions", "networking.k8s.io"],
+                resources: ["targetgroupbindings/status", "pods/status", "services/status", "ingresses/status"],
+                verbs: ["patch", "update"]
+            },
+            {
+                apiGroups: ["discovery.k8s.io"],
+                resources: ["endpointslices"],
+                verbs: ["get", "list", "patch", "watch"]
+            }
+        ],
+    }, {provider: k8sProvider, dependsOn: createdCluster})
+
+    const clusterRoleBinding = new k8s.rbac.v1.ClusterRoleBinding("aws-load-balancer-controller-rolebinding", {
+        metadata: {
+            name: "aws-load-balancer-controller-rolebinding",
+            namespace: "kube-system"
+        },
+        subjects: [{
+            kind: "ServiceAccount",
+            name: irsa.name,
+            namespace: "kube-system"
+        }],
+        roleRef: {
+            kind: "ClusterRole",
+            name: clusterRole.metadata.name,
+            apiGroup: "rbac.authorization.k8s.io"
         }
+    })
+}
